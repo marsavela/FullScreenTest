8b61ac00502e8a2a43c4c1e9a37195da98b37914
Sergiu Daniel Marsavela <marsavela@gmail.com>
2014-02-08 04:04:59 +0100

    Fixed ViewPager looping. ( https://github.com/antonyt/InfiniteViewPager ) Added gesturesDetector for touch events. Max brightness set while working with the app.

--- FullscreenTest/src/main/java/la/marsave/fullscreentest/MainActivity.java
+++ FullscreenTest/src/main/java/la/marsave/fullscreentest/MainActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -21,52 +25,89 @@ public class MainActivity extends Activity {
      * may be best to switch to a
      * {@link android.support.v13.app.FragmentStatePagerAdapter}.
      */
-    SectionsPagerAdapter mSectionsPagerAdapter;
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
-    ViewPager mViewPager;
+    private InfiniteViewPager mInfiniteViewPager;
+
+    /**
+     * The {@link int} that will host displayed colors.
+     */
+    private static final int[] colours = new int[] {
+            Color.WHITE,    Color.GRAY,     Color.BLACK,
+            Color.YELLOW,   Color.MAGENTA,  Color.CYAN,
+            Color.BLUE,     Color.GREEN,    Color.RED
+    };
+
+    // TAGS
+    private static final String TAG = "ImmersiveModeFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowManager.LayoutParams layout = getWindow().getAttributes();
+        layout.screenBrightness = 1F;
+        getWindow().setAttributes(layout);
         setContentView(R.layout.activity_main);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
+
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
-        mViewPager.setCurrentItem(9);
-        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i2) {
+        mInfiniteViewPager = (InfiniteViewPager) findViewById(R.id.pager);
+        mInfiniteViewPager.setAdapter(new InfinitePagerAdapter(mSectionsPagerAdapter));
+        mInfiniteViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mInfiniteViewPager.setGestureDetector(new GestureDetector(this, new GestureListener()));
+    }
 
-            }
+    @Override
+    protected void onResume() {
+        hideBars();
+        super.onResume();
+    }
 
-            @Override
-            public void onPageSelected(int i) {
+    /**
+     * Detects and toggles immersive mode (also known as "hidey bar" mode).
+     */
+    public void hideBars() {
 
-            }
+        // BEGIN_INCLUDE (get_current_ui_flags)
+        // The UI options currently enabled are represented by a bitfield.
+        // getSystemUiVisibility() gives us that bitfield.
+        //int uiOptions = this.getWindow().getDecorView().getSystemUiVisibility();
+        int newUiOptions = 0;
+        // END_INCLUDE (get_current_ui_flags)
+        // BEGIN_INCLUDE (toggle_ui_flags)
+        Log.i(TAG, "Turning immersive mode mode on.");
 
-            @Override
-            public void onPageScrollStateChanged(int i) {
-                if (i == ViewPager.SCROLL_STATE_IDLE) {
-                    switch (mViewPager.getCurrentItem()) {
-                        case 0:
-                            mViewPager.setCurrentItem(9, false);
-                            break;
-                        case 10:
-                            mViewPager.setCurrentItem(1, false);
-                    }
-                }
-            }
-        });
+        // Navigation bar hiding:  Backwards compatible to ICS.
+        if (Build.VERSION.SDK_INT >= 14) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        }
 
+        // Status bar hiding: Backwards compatible to Jellybean
+        if (Build.VERSION.SDK_INT >= 16) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
+        }
+
+        // Immersive mode: Backward compatible to KitKat.
+        // Note that this flag doesn't do anything by itself, it only augments the behavior
+        // of HIDE_NAVIGATION and FLAG_FULLSCREEN.
+        // All three flags are being toggled together.
+        // Note that there are two immersive mode UI flags, one of which is referred to as "sticky".
+        // Sticky immersive mode differs in that it makes the navigation and status bars
+        // semi-transparent, and the UI flag does not get cleared when the user interacts with
+        // the screen.
+        if (Build.VERSION.SDK_INT >= 18) {
+            newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        }
+
+        this.getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
+        //END_INCLUDE (set_ui_flags)
     }
 
     /**
@@ -83,13 +124,13 @@ public class MainActivity extends Activity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position);
+            return PlaceholderFragment.newInstance(colours[position]);
         }
 
         @Override
         public int getCount() {
-            // Show 11 total pages.
-            return 11;
+            // Show 9 total pages.
+            return colours.length;
         }
     }
 
@@ -101,16 +142,16 @@ public class MainActivity extends Activity {
          * The fragment argument representing the section number for this
          * fragment.
          */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final String ARG_COLOR_NUMBER = "color_number";
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int colorNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putInt(ARG_COLOR_NUMBER, colorNumber);
             fragment.setArguments(args);
             return fragment;
         }
@@ -120,44 +161,11 @@ public class MainActivity extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
-                case 0:
-                    rootView.setBackgroundColor(Color.RED);
-                    break;
-                case 1:
-                    rootView.setBackgroundColor(Color.WHITE);
-                    break;
-                case 2:
-                    rootView.setBackgroundColor(Color.GRAY);
-                    break;
-                case 3:
-                    rootView.setBackgroundColor(Color.BLACK);
-                    break;
-                case 4:
-                    rootView.setBackgroundColor(Color.YELLOW);
-                    break;
-                case 5:
-                    rootView.setBackgroundColor(Color.MAGENTA);
-                    break;
-                case 6:
-                    rootView.setBackgroundColor(Color.CYAN);
-                    break;
-                case 7:
-                    rootView.setBackgroundColor(Color.BLUE);
-                    break;
-                case 8:
-                    rootView.setBackgroundColor(Color.GREEN);
-                    break;
-                case 9:
-                    rootView.setBackgroundColor(Color.RED);
-                    break;
-                case 10:
-                    rootView.setBackgroundColor(Color.WHITE);
-                    break;
-            }
+            rootView.setBackgroundColor(getArguments().getInt(ARG_COLOR_NUMBER));
             return rootView;
         }
     }
+
 }
